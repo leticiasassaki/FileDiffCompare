f271c78f1a27a6f999c6209dd8d87c26711dc175
Leticia Kawamoto <leticiac.0401@gmail.com>
2021-10-12 11:36:31 -0300

    Simple file text compare

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Start!")
+const fs = require('fs');
+
+var fileText1 = fs.readFileSync('./files/file1.txt', 'utf-8');
+var fileText2 = fs.readFileSync('./files/file2.txt', 'utf-8');
+
+console.log(fileText1);
+console.log(fileText2);
+console.log(fileText1 === fileText2);
