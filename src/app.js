f36ab4b1ca412a77c0076fc6bb2556f3f1e0d883
Rodrigo Chibana <rodrigo.chibana@gmail.com>
2021-10-31 22:09:40 -0300

    Add express routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const fileComparatorService = require('./server/service/file-comparator')
+const filesRoute = require('./server/route/files')
 
 const app = express();
 const port = 3000
 
-app.get('/', async (req, res) => {
-    const result = await fileComparatorService();
-    console.log(result);
-    res.send('Hello World!')
-})
+app.use(filesRoute)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
