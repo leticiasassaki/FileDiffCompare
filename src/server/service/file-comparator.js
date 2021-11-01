b7f5b550aeae2f7e95a9bb1568b6953215627ee9
Rodrigo Chibana <rodrigo.chibana@gmail.com>
2021-10-31 21:35:45 -0300

    fix service

--- src/server/service/file-comparator.js
+++ src/server/service/file-comparator.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 
 const fileComparatorService = async () => {
 
-    const fileText1 = fs.readFileSync('files/file1.txt', 'utf-8');
-    const fileText2 = fs.readFileSync('files/file2.txt', 'utf-8');
+    const fileText1 = fs.readFileSync('./src/files/file1.txt', 'utf-8');
+    const fileText2 = fs.readFileSync('./src/files/file2.txt', 'utf-8');
 
     console.log(fileText1);
     console.log(fileText2);
